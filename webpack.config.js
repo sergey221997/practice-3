3234a28d6db5013b1c676ccaf912b9dd8dccbc0e
Sergey Doronin <sergeydoronin221997@gmail.com>
2022-10-06 16:38:09 +0500

    added file structure

---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/styles.scss',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        publicPath: ''
+    },
+    mode: 'development',
+    devServer: {
+        hot: true,
+        open: true,
+        port: 7777
+    },
+    module: {
+        rules: [
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            },
+            {
+                test: /\.s?[ac]ss$/,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpeg|jpg|gif|)$/i,
+                type: 'asset/resource'
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
